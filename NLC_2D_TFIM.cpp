726bca79b55e22b61f01a9f2005cf678858c9430
Katie Hyatt <kslimes@gmail.com>
2012-07-26 13:36:27 -0400

    Restored NLC_2D_TFIM and makefile2d-cpu to MPI-free versions

--- NLC_2D_TFIM.cpp
+++ NLC_2D_TFIM.cpp
@@ -21,16 +21,8 @@ using namespace std;
 #include "CPU/GenHam.h"
 #include "CPU/simparam.h"
 #include "graphs.h"
-#include <mpi.h>
 
 int main(int argc, char** argv){
-
-    MPI_Init(&argc, &argv);
-
-    int rank, size;
-    MPI_Status status;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     int CurrentArg = 1;
     string InputFile;
@@ -47,64 +39,68 @@ int main(int argc, char** argv){
         }
         CurrentArg++;
     }
-    
+
     double energy;
 
     PARAMS prm;  //Read parameters from param.dat  : see simparam.h
-    double J = 1.;
+    double J;
+    double h;
 
     vector <long double> eVec;
+
+    J=prm.JJ_;
+    h=prm.hh_;
 
     vector< graph > fileGraphs; //graph objects
     
     vector<double> WeightHigh;
     
     ReadGraphsFromFile(fileGraphs, InputFile);
+
+    ofstream fout(OutputFile.c_str());
+    fout.precision(10);
+    cout.precision(10);
     
-    if ( rank == 0 ) 
-    {
-        ofstream fout(OutputFile.c_str());
-        fout.precision(10);
-        double* Results = (double*)malloc((size - 1)*sizeof(double));
-        for( int i = 0; i < size - 1; i++ )
-        {
-            MPI_Recv(Results + i, 1, MPI_DOUBLE, i + 1, 0, MPI_COMM_WORLD, &status); //grab results from other processes
-            fout<<"h = "<<(i + 1)/J<<" J = "<<J<<" Energy = "<<Results[i]<<endl;
-        }
-        fout.close();
-    }
+    J=1;
     
-    double h = (double)rank/J;
+    for(int hh=1; hh<10; hh++){
+      h = hh;
       
-    WeightHigh.push_back(-h); //Weight for site zero
-    double RunningSumHigh = WeightHigh[0];      
-    
-    if ( rank )
-    {
-        for (int i = 1; i < fileGraphs.size(); i++)
-        { //skip the zeroth graph
+      WeightHigh.push_back(-h); //Weight for site zero
+      double RunningSumHigh = WeightHigh[0];      
+      
+      for (int i=1; i<fileGraphs.size(); i++){ //skip the zeroth graph
 	
-	    //---High-Field---
-	        GENHAM HV(fileGraphs.at(i).NumberSites, J, h, fileGraphs.at(i).AdjacencyList, fileGraphs.at(i).LowField); 
-
-            LANCZOS lancz(HV.Vdim);  //dimension of reduced Hilbert space (Sz sector)
-    
-            HV.SparseHamJQ();  //generates sparse matrix Hamiltonian for Lanczos
-            energy = lancz.Diag(HV, 1, 1, eVec); // Hamiltonian, # of eigenvalues to converge, 1 for -values only, 2 for vals AND vectors
-            WeightHigh.push_back(energy);
-    
-            for (int j = 0; j < fileGraphs[ i ].SubgraphList.size(); j++)
-            {
-	            WeightHigh.back() -= fileGraphs[ i ].SubgraphList[ j ].second * WeightHigh[ fileGraphs[ i ].SubgraphList[ j ].first ];
-            }
-
-            RunningSumHigh += fileGraphs[ i ].LatticeConstant * WeightHigh.back();
 	
-        }
-        MPI_Send( &RunningSumHigh, 1, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD); //send resulting sum to controller process
+	//---High-Field---
+	GENHAM HV(fileGraphs.at(i).NumberSites,J,h,fileGraphs.at(i).AdjacencyList,fileGraphs.at(i).LowField); 
 
+        LANCZOS lancz(HV.Vdim);  //dimension of reduced Hilbert space (Sz sector)
+        HV.SparseHamJQ();  //generates sparse matrix Hamiltonian for Lanczos
+        energy = lancz.Diag(HV, 1, 1, eVec); // Hamiltonian, # of eigenvalues to converge, 1 for -values only, 2 for vals AND vectors
+        WeightHigh.push_back(energy);
+        for (int j = 0; j < fileGraphs[ i ].SubgraphList.size(); j++)
+	  WeightHigh.back() -= fileGraphs[ i ].SubgraphList[ j ].second * WeightHigh[ fileGraphs[ i ].SubgraphList[ j ].first ];
+
+        cout<<"h="<<h<<" J="<<J<<" graph #"<<i<<"  ";
+        cout<<" energy "<<setprecision(12)<<energy<<endl;
+	//        cout<<"WeightHigh["<<i<<"] = "<<WeightHigh.back()<<endl;
+	    RunningSumHigh += fileGraphs[ i ].LatticeConstant * WeightHigh.back();
+        cout <<"RunningSumHigh = "<< RunningSumHigh;
+        cout<<endl;
+	
+      }
+      
+      fout<<"h= "<<h<<" J= "<<J;	
+      fout <<" Energy= "<< RunningSumHigh<< endl<<endl;
+      
+      WeightHigh.clear();
+      RunningSumHigh=0;
     }
-
+    
+    
     return 0;
+    
+    fout.close();
 
 }
